9db30d049c5cf2f3a8e1f91b4764c6d8c29ba239
Kevin Jin <kevinjin77@gmail.com>
2019-07-05 19:13:14 -0700

    Start laying out form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import CourseInfo from './components/CourseInfo'
 
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
@@ -19,7 +21,20 @@ function App() {
             </Typography>
           </Toolbar>
         </AppBar>
-        <div className='inputForm'>
+        <Grid container spacing={0} className='inputForm'>
+          <Grid item xs={6}>
+            <Paper className='courses'>
+              <h2>Course Information</h2>
+              <CourseInfo />
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className='preferences'>
+            <h2>Preferences</h2>
+            </Paper>
+          </Grid>
+        </Grid>
+        {/* <div className='inputForm'>
           <div className='courses'>
             <Paper className='paper'>
               Test
@@ -30,7 +45,7 @@ function App() {
               Test 2
             </Paper>
           </div>
-        </div>
+        </div> */}
         <div className='submitSection'>
           <Button variant="contained" color="primary">
             Generate Schedules
---
+++ src/components/CourseInfo.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const styles = {
+};
+
+class CourseInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        form: {
+          term: '',
+          numCourses: 2
+        }
+    };
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [name]: event.target.value
+      }
+    })
+  };
+
+  render() {
+    const { form: {term, numCourses} } = this.state;
+    return (
+      <form noValidate autoComplete="off">
+        <TextField
+          id="term"
+          label="Term"
+          value={term}
+          onChange={this.handleChange('term')}
+          margin="normal"
+        />
+        <br/>
+        <FormControl>
+          <InputLabel htmlFor="age-simple">Age</InputLabel>
+          <Select
+            value={numCourses}
+            onChange={this.handleChange('numCourses')}
+            inputProps={{
+              name: 'age',
+              id: 'age-simple',
+            }}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </form>
+    );
+  }
+}
+
+export default CourseInfo;
